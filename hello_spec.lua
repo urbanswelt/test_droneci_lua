848d13f634c7aa38236558d9f955f59874dbc36e
Lars Urban <urbanswelt@gmail.com>
2020-06-10 21:31:41 +0200

    hello_spec.lua:4:1: line contains only whitespace

--- hello_spec.lua
+++ hello_spec.lua
@@ -1,7 +1,7 @@
 -- spec/hello_spec.lua
- 
+
 local hello = require('hello')
- 
+
 describe('hello lib', function()
   it('says hello to you', function()
       assert.equal('Hello you', hello.say('you'))
